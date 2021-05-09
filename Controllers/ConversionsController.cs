c75c27900c19427bc22eb36d59e6c66c30128e31
LucasMiguel <lucasoliveira129@hotmail.com>
2021-05-09 20:35:08 -0300

    Initial Commit

---
+++ Controllers/ConversionsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ApiPocketEng.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ConversionsController : ControllerBase
+    {
+        
+        [HttpGet("{type}/{unity1}/{unity2}/{value}")]
+        public String Valor(string type, string unity1, string unity2, string value)
+        {
+
+            return $"Tipo {type} - Unidade 1 {unity1} - Unidade 2 {unity2} - Valor{value}";
+        }
+
+    }   
+    
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# API pública de Conversão de unidades
+
+## Para o acesso
+
+(url)/api/Conversions/TypeConvesion/Unity1/Unity2/value
