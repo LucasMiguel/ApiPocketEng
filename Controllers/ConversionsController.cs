eb8c7d32792b47770f711389bcbea2191f5e47d5
LucasMiguel <lucasoliveira129@hotmail.com>
2021-05-09 21:57:41 -0300

    Finalizado as conversões de área e ângulo

--- Controllers/ConversionsController.cs
+++ Controllers/ConversionsController.cs
@@ -15,8 +15,50 @@ namespace ApiPocketEng.Controllers
         [HttpGet("{type}/{unity1}/{unity2}/{value}")]
         public String Valor(string type, string unity1, string unity2, string value)
         {
+            String result = "";
 
-            return $"Tipo {type} - Unidade 1 {unity1} - Unidade 2 {unity2} - Valor{value}";
+            switch(type){
+                case "Area": result = Area(unity1, unity2, value); break;
+                case "Angulo": result = Angulo(unity1, unity2, value); break;
+                case "Comprimento": break;
+                case "Energia": break;
+                case "Pressao": break;
+                case "Potencia": break;
+                case "Temperatura": break;
+                case "Tempo": break;
+                case "Velocidade": break;
+                case "Volume": break;
+                default: return "Inválido";
+            }            
+
+            return result;
+        }
+
+        private string Area(string unity1, string unity2, string value){
+            double valueDouble = Convert.ToDouble(value);
+            int constant = 1000000;
+            if(unity1 == "m" && unity2 == "mm"){
+                return (valueDouble * constant).ToString("");
+            }else if(unity1 == "mm" && unity2 == "m"){
+                return (valueDouble / constant).ToString("");
+            }else if(unity1 == unity2){
+                return value;        
+            }else{
+                return "Conversão inválida!!";
+            }
+        }
+
+        private string Angulo(string unity1, string unity2, string value){
+            double valueDouble = Convert.ToDouble(value);            
+            if(unity1 == "graus" && unity2 == "rad"){
+                return (valueDouble*(Math.PI/180)).ToString("");
+            }else if(unity1 == "rad" && unity2 == "graus"){
+                return (valueDouble*(180/Math.PI)).ToString("");
+            }else if( unity1 == unity2){
+                return value; 
+            }else{
+                return "Conversão inválida!!";
+            }
         }
 
     }   
--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 ## Para o acesso
 
 (url)/api/Conversions/TypeConvesion/Unity1/Unity2/value
+Exemplo: (url)/api/Conversions/Area/m/mm/10 => para conversão de metros para milimetros
+
+
+### Conversões possíveis
+ - Área (Metros Quadrados, Milímetros Quadrados)
+ - Ángulo (Graus, Radianos)
+ - Comprimento (Metros, Milímetros, Centímetros, Polegadas)
+ - Energia (Watts/Hora, Joule)
+ - Pressao (Bar, Atm, Pascal, PSI)
+ - Potencia (Watts, Horse-power)
+ - Temperatura (Celsius,Fahrenheit, Kelvin)
+ - Tempo (Hora, Minutos, Segundos)
+ - Velocidade (Metros/Segundo, Quilometros/Hora)
+ - Volume (Litros, Metros Cúbicas, Polegadas Cúbicas)
